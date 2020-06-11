37728b9acea5d702905ad0b73b247fc6b0a7765f
Xiaohan Wang <xw176@cs.duke.edu>
2020-06-10 21:36:43 -0400

    add resnet50

--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -1,5 +1,7 @@
-from .resnet import build_ResNet101
+from .resnet import build_ResNet101, build_ResNet50
 
 def build_backbone(backbone, output_stride):
-    if backbone == 'resnet':
+    if backbone == 'resnet101':
         return build_ResNet101(output_stride=output_stride, pretrained=True)
+    elif backbone == 'resnet50':
+        return build_ResNet50(output_stride=output_stride, pretrained=True)
